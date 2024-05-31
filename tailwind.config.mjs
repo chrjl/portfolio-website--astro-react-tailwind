ed4a876a7463b640d3da33e60ee16a2a33c0dab6
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-31 01:21:37 -0700

    build: self-host fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+        content: ['Arimo', ...defaultTheme.fontFamily.sans],
+        mono: ['Cousine', ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
