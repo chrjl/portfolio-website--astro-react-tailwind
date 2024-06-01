ba01d054e9388534d9f43bb47911ce812d7d24f1
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-31 22:38:42 -0700

    feat(Card): create component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  title: string;
+}
+
+export default function Card({ title, keywords, description, links }: Props) {
+  return (
+    <div className="group border-solid border-2 border-black p-4 m-8 bg-gradient-to-b hover:from-gray-300 hover:to-gray-100">
+      <div className="text-center font-semibold group-hover:font-bold">
+        <h1 className="text-lg uppercase leading-snug underline">{title}</h1>
+      </div>
+    </div>
+  );
+}
