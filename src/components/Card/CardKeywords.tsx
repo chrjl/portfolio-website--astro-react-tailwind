688da3c429cfa5c5e5f595527de297b5966201d2
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-31 22:39:08 -0700

    feat(Card): add keywords to component header

---
+++ src/components/Card/CardKeywords.tsx
@@ -0,0 +1,15 @@
+interface CardKeywordsProps {
+  keywords: string[];
+}
+
+export default function CardKeywords({ keywords }: CardKeywordsProps) {
+  return (
+    <ul className="mt-3 ml-0 text-xs flex justify-center">
+      {keywords.map((keyword) => (
+        <li key={keyword} className="mr-3 inline-block uppercase last:mr-0">
+          {keyword}
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,7 @@
+import CardKeywords from './CardKeywords';
 interface Props {
   title: string;
+  keywords?: string[];
 }
 
 export default function Card({ title, keywords, description, links }: Props) {
@@ -7,6 +9,7 @@ export default function Card({ title, keywords, description, links }: Props) {
     <div className="group border-solid border-2 border-black p-4 m-8 bg-gradient-to-b hover:from-gray-300 hover:to-gray-100">
       <div className="text-center font-semibold group-hover:font-bold">
         <h1 className="text-lg uppercase leading-snug underline">{title}</h1>
+        {keywords && <CardKeywords keywords={keywords} />}
       </div>
     </div>
   );
