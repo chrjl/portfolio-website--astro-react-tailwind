92fcb278378625ae4b9790b778a6ea5485e4e4b1
chrjl <73675826+chrjl@users.noreply.github.com>
2024-06-01 18:32:41 -0700

    feat(CardLinks): define iconMap, add icons to links

--- src/components/Card/CardLinks.tsx
+++ src/components/Card/CardLinks.tsx
@@ -1,3 +1,22 @@
+import {
+  ArrowTopRightOnSquareIcon,
+  CodeBracketSquareIcon,
+  DocumentTextIcon,
+  LinkIcon,
+} from '@heroicons/react/24/outline';
+
+import githubMark from '../../assets/github-mark.svg';
+
+const iconMap: { [type: string]: React.ReactNode } = {
+  repo: (
+    <img className="inline-block align-middle h-full" src={githubMark.src} />
+  ),
+  asset: <ArrowTopRightOnSquareIcon className="h-full" />,
+  code: <CodeBracketSquareIcon className="h-full" />,
+  doc: <DocumentTextIcon className="h-full" />,
+  generic: <LinkIcon className="h-full" />,
+};
+
 export interface LinksType {
   type: string;
   url: string;
@@ -18,25 +37,32 @@ export default function CardLinks({ links }: Props) {
   return [
     <div>
       {otherLinks.map(({ type, url, description }) => (
-        <IconLink icon={iconMap[type]} url={url} description={description} />
+        <IconLink
+          key={url}
+          icon={iconMap[type]}
+          url={url}
+          description={description}
+        />
       ))}
     </div>,
     <div className="mt-4">
       {repos.map(({ type, url }) => (
-        <IconLink icon={iconMap[type]} url={url} />
+        <IconLink key={url} icon={iconMap[type]} url={url} />
       ))}
     </div>,
   ];
 }
 
 interface IconLinkProps {
+  icon: React.ReactNode;
   url: string;
   description?: string;
 }
 
-function IconLink({ url, description }: IconLinkProps) {
+function IconLink({ icon, url, description }: IconLinkProps) {
   return (
     <div className="flex flex-row items-center">
+      <span className="h-4 w-6 flex">{icon || iconMap.generic}</span>
       {description ? (
         <a href={url} className="text-sm">
           {description}
