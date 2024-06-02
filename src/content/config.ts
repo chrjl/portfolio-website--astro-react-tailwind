b963f4b74149986e5464ba59d65e26523bdb778d
chrjl <73675826+chrjl@users.noreply.github.com>
2024-06-01 19:47:15 -0700

    refactor(content): rename stubs collection => posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,26 @@
 import { z, defineCollection } from 'astro:content';
 
-const stubsCollection = defineCollection({
+const postsCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    order: z.number(),
+    order: z.number().optional(),
     created_date: z.date().optional(),
     modified_date: z.date().optional(),
     keywords: z.array(z.string()).optional(),
+    description: z.string().optional(),
     links: z
-      .object({
-        type: z.enum(['repo', 'doc', 'asset', 'code', 'link', 'target']),
-        url: z.string(),
-        description: z.string().optional(),
-      })
+      .array(
+        z.object({
+          type: z.string(),
+          url: z.string(),
+          description: z.string().optional(),
+        })
+      )
       .optional(),
   }),
 });
+
+export const collections = {
+  posts: postsCollection,
+};
