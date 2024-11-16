00d8a1dc78ccd828da7dccb64654eb079c30729d
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-15 17:05:33 -0800

    refactor(Header): read fields from a metadata file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,60 @@
-import githubMark from '../assets/github-mark.svg';
-import nodeLogo from '../assets/nodejs-stacked-dark.svg';
-import reactLogo from '../assets/react.svg';
-import tsLogo from '../assets/ts-logo-128.svg';
+import { FaGithub, FaLinkedin } from 'react-icons/fa6';
 
-export default function Header() {
-  return (
-    <header className="my-8 text-center">
-      <h1 className="text-4xl font-extrabold">Christopher Lee</h1>
+interface Props {
+  title: string;
+  subtitle: string;
+  subsubtitle: string;
+  description: string;
+  links: {
+    url?: string;
+    type?: string;
+    profile?: string;
+  }[]
+}
 
-      <h2 className="mt-4 text-xl font-bold">
-        JavaScript Developer
-        <span className="ml-4 inline-block [&>a]:ml-1.5 [&_img]:inline [&_img]:h-6">
-          <a href="https://nodejs.org/about" target="_blank">
-            <img src={nodeLogo.src} alt="node" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo.src} alt="react" />
-          </a>
-          <a href="https://typescriptlang.org" target="_blank">
-            <img src={tsLogo.src} alt="typescript" />
+export default function Header({
+  title,
+  subtitle,
+  subsubtitle,
+  description,
+  links,
+}: Props) {
+  const linkElements = links.map((link) => {
+    if (link.type === 'GitHub') {
+      return (
+        <span className="flex items-center">
+          <FaGithub />
+          <a className="pl-1" href={`https://github.com/${link.profile}`}>
+            {link.profile}
           </a>
         </span>
+      );
+    } else if (link.type === 'LinkedIn') {
+      return (
+        <span className="flex items-center">
+          <FaLinkedin />
+          <a className="pl-1" href={`https://linkedin.com/in/${link.profile}`}>
+            {link.profile}
+          </a>
+        </span>
+      );
+    }
 
-        <div className="mt-1 text-base font-medium">
-          AWS Certified Solutions Architect Associate
-        </div>
-      </h2>
+    return <a href={link.url}>{link.url}</a>;
+  });
 
-      <h3 className="mt-4 text-sm [&>*]:align-middle">
-        <span>Los Angeles, California</span>
-        <span className="px-4">|</span>
-        <img className="mr-1 inline h-4" src={githubMark.src} alt="[github]" />
-        <a href="https://github.com/chrjl" target="_blank">
-          chrjl
-        </a>
+  return (
+    <header className="my-8 flex flex-col items-center">
+      <h1 className="text-4xl font-extrabold">{title}</h1>
+      <h2 className="mt-4 text-xl font-bold">{subtitle}</h2>
+      <div className="mt-1 text-base font-medium">{subsubtitle}</div>
+
+      <h3 className="mt-4 text-sm flex flex-wrap justify-center">
+        {[description, ...linkElements].map((element, index) => (
+          <>
+            {index === 0 ? null : <span className="px-4">|</span>} {element}
+          </>
+        ))}
       </h3>
     </header>
   );
