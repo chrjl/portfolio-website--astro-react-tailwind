584e93ef655e87c7c674bdcd4d54d89583db2a25
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-16 09:18:28 -0800

    feat(Card): add className prop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,11 +9,20 @@ interface Props {
   keywords?: string[];
   description?: string;
   links?: Link[];
+  className?: string;
 }
 
-export default function Card({ title, keywords, description, links }: Props) {
+export default function Card({
+  title,
+  keywords,
+  description,
+  links,
+  className,
+}: Props) {
   return (
-    <div className="group border-solid border-2 border-black p-4 m-8 bg-gradient-to-b hover:from-gray-300 hover:to-gray-100">
+    <div
+      className={`group border-solid border-2 border-black p-4 m-8 bg-gradient-to-b hover:from-gray-300 hover:to-gray-100 ${className || ''}`}
+    >
       <div className="text-center font-semibold group-hover:font-bold">
         <h1 className="text-lg uppercase leading-snug underline">{title}</h1>
         {keywords && <CardKeywords keywords={keywords} />}
