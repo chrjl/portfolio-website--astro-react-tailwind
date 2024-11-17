c2a10786f1462f493744aa83f3d029cc3fb4ab27
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-16 20:33:46 -0800

    fix(Header): types

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa6';
 
 interface Props {
   title: string;
-  subtitle: string;
-  subsubtitle: string;
-  description: string;
-  links: {
+  subtitle?: string;
+  subsubtitle?: string;
+  description?: string;
+  links?: {
     url: string;
     type?: string;
     profile?: string;
@@ -19,35 +19,48 @@ export default function Header({
   description,
   links,
 }: Props) {
-  const linkElements = links.map((link) => {
-    if (link.type === 'GitHub') {
-      return (
-        <span className="flex items-center">
-          <FaGithub />
-          <a className="pl-1" href={`https://github.com/${link.profile}`}>
-            {link.profile}
-          </a>
-        </span>
-      );
-    } else if (link.type === 'LinkedIn') {
-      return (
-        <span className="flex items-center">
-          <FaLinkedin />
-          <a className="pl-1" href={`https://linkedin.com/in/${link.profile}`}>
-            {link.profile}
-          </a>
-        </span>
-      );
-    }
+  const linkElements = links
+    ? links.map((link) => {
+        if (link.type === 'GitHub') {
+          return (
+            <span className="flex items-center">
+              <FaGithub />
+              <a className="pl-1" href={`https://github.com/${link.profile}`}>
+                {link.profile}
+              </a>
+            </span>
+          );
+        } else if (link.type === 'LinkedIn') {
+          return (
+            <span className="flex items-center">
+              <FaLinkedin />
+              <a
+                className="pl-1"
+                href={`https://linkedin.com/in/${link.profile}`}
+              >
+                {link.profile}
+              </a>
+            </span>
+          );
+        }
 
-    return <a href={link.url}>{link.url}</a>;
-  });
+        return <a href={link.url}>{link.url}</a>;
+      })
+    : [];
 
   return (
     <header className="my-8 flex flex-col items-center">
       <h1 className="text-4xl font-extrabold">{title}</h1>
-      <h2 className="mt-4 text-xl font-bold">{subtitle}</h2>
-      <div className="mt-1 text-base font-medium">{subsubtitle}</div>
+
+      {subtitle ? (
+        <h2 className="mt-4 text-xl font-bold">{subtitle}</h2>
+      ) : null}
+
+      {subsubtitle ? (
+        <div className="mt-1 text-base font-medium">
+          {subsubtitle}
+        </div>
+      ) : null}
 
       <h3 className="mt-4 text-sm flex flex-col justify-center items-center sm:hidden">
         {[description, ...linkElements].filter(Boolean)}
