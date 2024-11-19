147eee76da7046e62fee919165db7279c7239162
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-19 14:14:46 -0800

    feat(Card): show content on click

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Markdown from 'react-markdown';
 import rehypeExternalLinks from 'rehype-external-links';
 
@@ -10,6 +12,7 @@ interface Props {
   keywords?: string[];
   description?: string;
   links?: Link[];
+  content?: string;
   className?: string;
 }
 
@@ -18,11 +21,24 @@ export default function Card({
   keywords,
   description,
   links,
+  content,
   className,
 }: Props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleClick = (e: React.MouseEvent) => {
+    if (!(e.target instanceof HTMLElement) || e.target.nodeName === 'A') {
+      return;
+    }
+
+    setIsExpanded((isExpanded) => !isExpanded);
+  };
+
   return (
     <div
-      className={`group border-solid border-2 border-black p-4 m-8 bg-gradient-to-b hover:from-gray-300 hover:to-gray-100 ${className || ''}`}
+      className={`group ${content ? 'cursor-pointer' : 'cursor-auto'} border-solid border-2 border-black p-4 m-8 bg-gradient-to-b hover:from-gray-300 hover:to-gray-100 ${className || ''}`}
+      tabIndex={0}
+      onClick={handleClick}
     >
       <div className="text-center font-semibold group-hover:font-bold">
         <h1 className="text-lg uppercase leading-snug underline">{title}</h1>
@@ -35,6 +51,25 @@ export default function Card({
             rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }]]}
           >
             {description}
+          </Markdown>
+        </div>
+      )}
+
+      {content && (
+        <div
+          className={`mt-8 text-sm font-content group-hover:[&_a]:font-bold`}
+        >
+          <a
+            className={`${isExpanded ? 'hidden' : 'block'}`}
+            onClick={() => setIsExpanded(true)}
+          >
+            Read more...
+          </a>
+          <Markdown
+            className={`${isExpanded ? 'block' : 'hidden'} duration-1000`}
+            rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }]]}
+          >
+            {content}
           </Markdown>
         </div>
       )}
